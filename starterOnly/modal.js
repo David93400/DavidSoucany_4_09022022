081635946b647092e20fc2a4a6a2371b26e25f52
Soucany <soucany.david@gmail.com>
2022-03-02 14:14:29 +0100

    some changes and refactoring

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,8 +30,8 @@ const quantityCity = document.getElementById('quantity');
 const form = document.getElementById('form');
 const validation = document.getElementById('validation');
 
-const prenomError = document.querySelector('#errorFirst');
-const nomError = document.querySelector('#errorLast');
+const firstNameError = document.querySelector('#errorFirst');
+const lastNameError = document.querySelector('#errorLast');
 const mailError = document.querySelector('#errorMail');
 const birthdayError = document.querySelector('#errorBirthday');
 const quantityCityError = document.querySelector('#errorQuantityCity');
@@ -48,15 +48,15 @@ const regexNumber = /^([0-9]|[1-9][0-9])$/;
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// launch modal function
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
 // Close modal event
 modalClose.addEventListener("click", closeModal);
 
 
-// launch modal function
-function launchModal() {
-  modalbg.style.display = "block";
-  // setTimeout(resetValidation(spanError, 500))
-}
 // close modal function
 function closeModal() {
   inputField.forEach(function (input) {
@@ -90,7 +90,7 @@ const displayError = (field, fieldError) => {
 
 const validateField = (regex, field, fieldError, long=0) => {
   
-  if (regex.test(field.value) && field.value.length >= long) {
+  if (regex.test(field) && field.length >= long) {
     displaySuccess(field, fieldError)
     return true
   } 
@@ -99,49 +99,44 @@ const validateField = (regex, field, fieldError, long=0) => {
 }
 
 const validateRadio = (fieldError) => {
-  const checkedRadio = document.querySelector("input[name='location']:checked");
-  if (checkedRadio) {
-    fieldError.classList.add('success');
-    return true
-  } 
-  fieldError.classList.remove('success');
-  return false
-};
+  const checkedRadio = document.querySelector("input[name='location']:checked")
+  checkedRadio
+    ? fieldError.classList.add('success')
+    : fieldError.classList.remove('success')
+  return checkedRadio
+}
 
-
-let CGU = false;
+let CGU = false
 cguLabel.addEventListener('click', function () {
-  CGU = !CGU;
+  CGU = !CGU
 });
 
 const validateCheckbox = (fieldError) => {
-  if (CGU === true) {
-    fieldError.classList.add('success');
-    return true
-  } 
-  fieldError.classList.remove('success');
-  return false
+  CGU
+    ? fieldError.classList.add('success')
+    : fieldError.classList.remove('success')
+  return CGU
 };
 
-const getInput = () => {
-  const firstName = document.getElementById('first').value;
-  const lastName = document.getElementById('last').value;
-  const email = document.getElementById('email').value;
-  const birthday = document.getElementById('birthdate').value;
-  const quantityCity = document.getElementById('quantity').value;
-  const radio = document.querySelector("input[name='location']:checked");
+const getInputs = () => {
+  const firstName = document.getElementById('first').value
+  const lastName = document.getElementById('last').value
+  const email = document.getElementById('email').value
+  const birthday = document.getElementById('birthdate').value
+  const quantityCity = document.getElementById('quantity').value
+  const radio = document.querySelector("input[name='location']:checked")
   const cgu = document.querySelector('#checkbox1').checked;
-  return { firstName, lastName, email, birthday, quantityCity, radio, cgu };
-};
+  return { firstName, lastName, email, birthday, quantityCity, radio, cgu }
+}
 
-const checkForm = (formInput) => {
-  const isValid = {};
-  Object.entries(formInput).map((input) => {
+const checkForm = (formInputs) => {
+  const isValid = {}
+  Object.entries(formInputs).map((input) => {
     const key = input[0]
     if (key === 'firstName')
-      isValid[key] = validateField(regexName, firstName, prenomError, 2);
+      isValid[key] = validateField(regexName, firstName, firstNameError, 2);
     if (key === 'lastName')
-      isValid[key] = validateField(regexName, lastName, nomError, 2);
+      isValid[key] = validateField(regexName, lastName, lastNameError, 2);
     if (key === 'email')
       isValid[key] = validateField(regexMail, email, mailError);
     if (key === 'birthday')
@@ -155,29 +150,29 @@ const checkForm = (formInput) => {
     if (key === 'radio') isValid[key] = validateRadio(locationError);
     if (key === 'cgu') isValid[key] = validateCheckbox(CGUError);
   });
-  const wrongInput = Object.entries(isValid).filter(([key, value]) => !value);
-  return !wrongInput.length;
+  const wrongInputs = Object.entries(isValid).filter(([key, value]) => !value)
+  return !wrongInputs.length
 };
 
-firstName.addEventListener("focusout", function(){validateField(regexName, firstName, prenomError, 2)})
-lastName.addEventListener("focusout", function(){validateField(regexName, lastName, nomError, 2)})
-email.addEventListener("focusout", function(){validateField(regexMail, email, mailError)})
-birthday.addEventListener("focusout", function(){validateField(regexDate, birthday, birthdayError)})
-quantityCity.addEventListener("focusout", function(){validateField(regexNumber, quantityCity, quantityCityError)})
-formRadio.addEventListener("click", function(){validateRadio(locationError)})
-submit.addEventListener("click", function(){validateRadio(locationError)})
-CGUForm.addEventListener('click', function(){validateCheckbox(CGUError);});
-submit.addEventListener("click", function(){validateCheckbox(CGUError)})
+firstName.addEventListener("focusout", () => validateField(regexName, firstName, firstNameError, 2))
+lastName.addEventListener("focusout", () => validateField(regexName, lastName, lastNameError, 2))
+email.addEventListener("focusout", () => validateField(regexMail, email, mailError))
+birthday.addEventListener("focusout", () => validateField(regexDate, birthday, birthdayError))
+quantityCity.addEventListener("focusout", () => validateField(regexNumber, quantityCity, quantityCityError))
+formRadio.addEventListener("click", () => validateRadio(locationError))
+submit.addEventListener("click", () => validateRadio(locationError))
+CGUForm.addEventListener('click', () => validateCheckbox(CGUError))
+submit.addEventListener("click", () => validateCheckbox(CGUError))
 
 
 submit.addEventListener("click", function(e) {
-  e.preventDefault();
-  const input = getInput();
-  const formIsValid = checkForm(input);
+  e.preventDefault()
+  const inputs = getInputs()
+  const formIsValid = checkForm(inputs);
   if (!formIsValid) {
     return false
   }
-  form.classList.add('success');
-  validation.classList.remove('success');
+  form.classList.add('success')
+  validation.classList.remove('success')
 })
 
